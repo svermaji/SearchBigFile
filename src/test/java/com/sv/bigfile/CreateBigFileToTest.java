62019431b4c02949c1f47b3eaa161af5d6968467
Shailendra Verma <shailendravermag@gmail.com>
2021-11-27 15:51:12 +0000

    update for msgs

--- src/test/java/com/sv/bigfile/CreateBigFileToTest.java
+++ src/test/java/com/sv/bigfile/CreateBigFileToTest.java
@@ -4,7 +4,8 @@ import com.sv.core.Utils;
 
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateBigFileToTest {
 
@@ -22,64 +23,66 @@ public class CreateBigFileToTest {
         String line2 = "This is test line used for creating big file # ";
         String line3 = "I am enjoying this # ";
 
-        String fn = "test-file-1GB.txt";
-        String fn3 = "test-file-3GB.txt";
-        String fn2 = "test-file-single-line.txt";
         StringBuilder sb = new StringBuilder("File created on " + Utils.getFormattedDate());
-        String[] arr = {fn, fn2, fn3};
-        StringBuilder finalSb = sb;
-        Arrays.stream(arr).forEach(f -> {
-            StandardOpenOption soo = Files.exists(Utils.createPath(f)) ?
-                    StandardOpenOption.TRUNCATE_EXISTING : StandardOpenOption.CREATE;
-            Utils.writeFile(f, finalSb.toString(), null, soo);
-        });
-
-        sb = new StringBuilder();
         int MB_14_LINES = 100000;
         int GB_1_LINES = MB_14_LINES * 65;
         int GB_3_LINES = MB_14_LINES * 200;
         int APPEND_AFTER = 100000;
-        long time = System.currentTimeMillis();
-        for (int i = 0; i < GB_1_LINES; i++) {
-            sb.append(ln).append(line1).append(i);
-            sb.append(ln).append(line2).append(i);
-            sb.append(ln).append(line3).append(i);
-            if (i % APPEND_AFTER == 0) {
-                Utils.writeFile(fn, sb.toString(), null, StandardOpenOption.APPEND);
-                sb = new StringBuilder();
+        long time;
+
+        String fnsf = "test-file-single-line.txt";
+        if (!Files.exists(Utils.createPath(fnsf))) {
+            Utils.writeFile(fnsf, sb.toString(), null, StandardOpenOption.APPEND);
+            time = System.currentTimeMillis();
+            sb = new StringBuilder();
+            for (int i = 0; i < MB_14_LINES / 2; i++) {
+                sb.append(line1).append(i);
+                sb.append(line2).append(i);
+                sb.append(line3).append(i);
+                if (i % APPEND_AFTER == 0) {
+                    Utils.writeFile(fnsf, sb.toString(), null, StandardOpenOption.APPEND);
+                    sb = new StringBuilder();
+                }
+            }
+            Utils.writeFile(fnsf, sb.toString(), null, StandardOpenOption.APPEND);
+            System.out.println("File [" + fnsf + "] created in " + Utils.getTimeDiffSecStr(time) + " of size "
+                    + Utils.getFileSizeString(fnsf));
+        } else {
+            System.out.println("File [" + fnsf + "] already exists of size "
+                    + Utils.getFileSizeString(fnsf));
+        }
+
+        String fn = "test-file-1GB.txt";
+        String fn2 = "test-file-500MB.txt";
+        String fn3 = "test-file-3GB.txt";
+        sb = new StringBuilder("File created on " + Utils.getFormattedDate());
+        Map<String, Integer> files = new HashMap<>();
+        files.put(fn, GB_1_LINES);
+        files.put(fn2, MB_14_LINES * 30);
+        files.put(fn3, GB_3_LINES);
+
+        for (Map.Entry<String, Integer> entry : files.entrySet()) {
+            String k = entry.getKey();
+            Integer v = entry.getValue();
+            time = System.currentTimeMillis();
+            if (!Files.exists(Utils.createPath(k))) {
+                for (int i = 0; i < v; i++) {
+                    sb.append(ln).append(line1).append(i);
+                    sb.append(ln).append(line2).append(i);
+                    sb.append(ln).append(line3).append(i);
+                    if (i % APPEND_AFTER == 0) {
+                        Utils.writeFile(fn, sb.toString(), null, StandardOpenOption.APPEND);
+                        sb = new StringBuilder();
+                    }
+                }
+                Utils.writeFile(k, sb.toString(), null, StandardOpenOption.APPEND);
+                System.out.println("File [" + k + "] created in " + Utils.getTimeDiffSecStr(time) + " of size "
+                        + Utils.getFileSizeString(k));
+            } else {
+                System.out.println("File [" + k + "] already exists of size "
+                        + Utils.getFileSizeString(k));
             }
         }
-        Utils.writeFile(fn, sb.toString(), null, StandardOpenOption.APPEND);
-        System.out.println("File [" + fn + "] created in " + Utils.getTimeDiffSecStr(time) + " of size "
-                + Utils.getFileSizeString(fn));
-        time = System.currentTimeMillis();
-        sb = new StringBuilder();
-        for (int i = 0; i < MB_14_LINES / 2; i++) {
-            sb.append(line1).append(i);
-            sb.append(line2).append(i);
-            sb.append(line3).append(i);
-            if (i % APPEND_AFTER == 0) {
-                Utils.writeFile(fn2, sb.toString(), null, StandardOpenOption.APPEND);
-                sb = new StringBuilder();
-            }
-        }
-        Utils.writeFile(fn2, sb.toString(), null, StandardOpenOption.APPEND);
-        System.out.println("File [" + fn2 + "] created in " + Utils.getTimeDiffSecStr(time) + " of size "
-                + Utils.getFileSizeString(fn2));
-        time = System.currentTimeMillis();
-        sb = new StringBuilder();
-        for (int i = 0; i < GB_3_LINES; i++) {
-            sb.append(ln).append(line1).append(i);
-            sb.append(ln).append(line2).append(i);
-            sb.append(ln).append(line3).append(i);
-            if (i % APPEND_AFTER == 0) {
-                Utils.writeFile(fn3, sb.toString(), null, StandardOpenOption.APPEND);
-                sb = new StringBuilder();
-            }
-        }
-        Utils.writeFile(fn3, sb.toString(), null, StandardOpenOption.APPEND);
-        System.out.println("File [" + fn3 + "] created in " + Utils.getTimeDiffSecStr(time) + " of size "
-                + Utils.getFileSizeString(fn3));
     }
 
 }
