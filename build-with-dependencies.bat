5856ab4f5134c5ce0e6e15de4846430476a08f07
Shailendra Verma <shailendravermag@gmail.com>
2021-12-04 14:53:46 +0000

    added batch file to build and run with dependencies

---
+++ build-with-dependencies.bat
@@ -0,0 +1,12 @@
+rem expecting all dependecies in same root folder
+set dir=%CD%
+cd ../Core-Parent
+call mvn install -DskiptTests=true
+cd ../Core
+call mvn install -DskiptTests=true
+cd ../SwingUI
+call mvn install -DskiptTests=true
+cd ../RunCommand
+call mvn install -DskiptTests=true
+cd %dir%
+call mvn install -DskiptTests=true
